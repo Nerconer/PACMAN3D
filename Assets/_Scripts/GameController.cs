2dd534e38c96d66c7ed5be78070dc367682635fe
davidS <david.sola.sole@gmail.com>
2016-05-25 03:18:00 +0200

    doing mapa1

--- Assets/_Scripts/GameController.cs
+++ Assets/_Scripts/GameController.cs
@@ -2,12 +2,23 @@
 using UnityEngine.UI;
 using System.Collections;
 
+public struct boxPosition {
+	public float x1;
+	public float x2;
+	public float z1;
+	public float z2;
+}
 
 public class GameController : MonoBehaviour {
 
+	public GameObject readyText;
+	public float moveSpeedReadyT = 5;
+
+	boxPosition bposL, bposR;
+
 	private int score;
 	private int highScore;
-	private int level = 1;
+	public int level = 1;
 	private int lives;
 
 	public Text testing;
@@ -19,8 +30,11 @@ public class GameController : MonoBehaviour {
 	private bool isEating;
 
 	int elapsed_time;
+	int starting_time;
 
 	float x, y;
+	float posx, posz;
+	bool entrat;
 
 	GameObject pacman;
 	GameObject map;
@@ -35,11 +49,57 @@ public class GameController : MonoBehaviour {
 		y = GameObject.Find("Pacman").GetComponent<Player>().y;
 		score = GameObject.Find ("Pacman").GetComponent<Player>().score;
 		lives = GameObject.Find("Pacman").GetComponent<Player>().lives;
-		//isEating = GameObject.Find("Pacman").GetComponent<Player>().isEating;
+		posx = GameObject.Find("Pacman").GetComponent<Player>().transform.position.x;
+		posz = GameObject.Find("Pacman").GetComponent<Player>().transform.position.z;
+	}
+
+	void detectTeleport() {
+		Vector3 temp;
+		if(posx < bposL.x2 && posx >= bposL.x1 && posz <= bposL.z2 && posz >= bposL.z1) {
+			entrat = true;
+			if(level == 1) {
+				temp = new Vector3(133, 5, -14);
+				pacman.GetComponent<Player>().transform.position = temp;
+			}
+			else if (level == 2){
+				//temp = new Vector3(133, 5, -14);
+				//pacman.GetComponent<Player>().transform.position = temp;
+			}
+		} else if(posx > bposR.x1 && posx <= bposR.x2 && posz <= bposR.z2 && posz >= bposR.z1) {
+			if(level == 1) {
+				temp = new Vector3(-185, 5, -14);
+				pacman.GetComponent<Player>().transform.position = temp;
+			}
+			else if (level == 2){
+				//temp = new Vector3(133, 5, -14);
+				//pacman.GetComponent<Player>().transform.position = temp;
+			}
+		} else {
+			entrat = false;
+		}
+	}
+		
+	void setLevel() {
+		if(level == 1) {
+			bposL.x1 = -190;
+			bposL.x2 = -188;
+			bposL.z1 = -16;
+			bposL.z2 = -10;
+
+			bposR.x1 = 138;
+			bposR.x2 = 140;
+			bposR.z1 = -16;
+			bposR.z2 = -10;
+
+		} else if(level == 2) {
+
+		}
 	}
 
 	// Use this for initialization
 	void Start () {
+		starting_time = (int)Time.time;
+		entrat = false;
 		starting = true;
 		getComponents();
 		highScore = 0;
@@ -48,30 +108,25 @@ public class GameController : MonoBehaviour {
 		map = GameObject.Find("Map");
 		pacmanScript = pacman.GetComponent<Player>();
 		pacmanScriptMusic = map.GetComponent<SoundController>();
+		setLevel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		//isEating = false;
-		elapsed_time = (int)Time.time;
+		elapsed_time = (int)Time.time - starting_time;
 		if(elapsed_time >= 4) {
 			pacmanScript.move = true;
 			starting = false;
+			readyText.SetActive(false);
 		}
 		getComponents();
 		printTestText();
 		printText();
 
 		if(starting) {	
-
+			readyText.transform.position += Vector3.up * Time.deltaTime * moveSpeedReadyT;
 		} else {	// End starting song
-			/*if(isEating) {
-				//Debug.Log("Entro");
-				pacmanScriptMusic.isEating = true;	// REVISAAAAAARRRR
-			} else {
-				pacmanScriptMusic.isEating = false;
-				//pacmanScriptMusic.beginingMusic.Play();
-			}*/
+			detectTeleport();
 		}
 	}
 
@@ -79,6 +134,9 @@ public class GameController : MonoBehaviour {
 	{
 		testing.text = "x: " + x.ToString() + '\n';
 		testing.text += "y: " + y.ToString() + '\n';
+		testing.text += "Position X: " + posx.ToString() + '\n';
+		testing.text += "Position Z: " + posz.ToString() + '\n';
+		testing.text += "Entrat: " + entrat.ToString() + '\n';
 	}
 
 	void printText()
--- Assets/_Scripts/SoundController.cs
+++ Assets/_Scripts/SoundController.cs
@@ -7,6 +7,7 @@ public class SoundController : MonoBehaviour {
 	public AudioSource eatingMusic;
 
 	private int elapsed_time;
+	private int starting_time;
 	private bool starting;
 
 	public bool isEating;
@@ -21,16 +22,18 @@ public class SoundController : MonoBehaviour {
 	void Start () {
 		elapsed_time = 0;
 		starting = true;
+		starting_time = (int)Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		elapsed_time = (int) Time.time;
-		if(elapsed_time >= 4) {
+		elapsed_time = (int) Time.time - starting_time;
+		if(elapsed_time >= 4 || starting == false) {
 			starting = false;
 		} else {
 			if(!beginingMusic.isPlaying) {
 				beginingMusic.Play();
+				starting = false;
 			}
 		}
 	}
